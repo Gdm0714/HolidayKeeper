0aea63d23d482e12163f1ef99aa5b0c617dbfc4e
Gdm0714 <ehdals45454@naver.com>
2025-06-20 14:09:29 +0900

    feat: holiday 엔티티 구현

---
+++ src/main/java/com/min/holidaykeeper/entity/Holiday.java
@@ -0,0 +1,55 @@
+package com.min.holidaykeeper.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(indexes = {@Index(name = "idx_holidays_year_country", columnList = "year, country_code")})
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Holiday {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "holiday_id")
+    private Long id;
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    private String localName;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String countryCode;
+
+    @NotNull
+    private boolean fixed;
+
+    @NotNull
+    private boolean global;
+
+    private String counties;
+
+    private Integer launchYear;
+
+    @ElementCollection
+    @CollectionTable(name = "holiday_types", joinColumns = @JoinColumn(name = "holiday_id"))
+    @Column(name = "type")
+    private Set<String> types = new HashSet<>();
+
+    private int year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_code", insertable = false, updatable = false)
+    private Country country;
+}
