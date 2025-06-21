9d2dc145c3508cc57dd75bb3f73b96450c16499d
Gdm0714 <ehdals45454@naver.com>
2025-06-21 10:03:12 +0900

    chore: @Schema로 description, example 추가

--- src/main/java/com/min/holidaykeeper/dto/request/HolidayRequest.java
+++ src/main/java/com/min/holidaykeeper/dto/request/HolidayRequest.java
@@ -8,13 +8,18 @@ import java.time.LocalDate;
 @Data
 public class HolidayRequest {
 
+    @Schema(description = "연도", example = "2025")
     private Integer holidayYear;
 
+    @Schema(description = "국가 코드", example = "KR")
     private String countryCode;
 
+    @Schema(description = "시작일", example = "2025-01-01")
     private LocalDate from;
 
+    @Schema(description = "종료일", example = "2025-12-31")
     private LocalDate to;
 
+    @Schema(description = "공휴일 타입", example = "Public")
     private String type;
 }
