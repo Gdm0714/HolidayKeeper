7d3aacab69dfce0be8e60eeeef3e004e6841ae21
Gdm0714 <ehdals45454@naver.com>
2025-06-21 20:02:30 +0900

    feat: jpql 삭제 및 custom repository 상속

--- src/main/java/com/min/holidaykeeper/repository/HolidayRepository.java
+++ src/main/java/com/min/holidaykeeper/repository/HolidayRepository.java
@@ -1,28 +1,13 @@
 package com.min.holidaykeeper.repository;
 
 import com.min.holidaykeeper.entity.Holiday;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 
-public interface HolidayRepository extends JpaRepository<Holiday, Long> {
-    @Query("SELECT h FROM Holiday h WHERE " +
-           "(:holidayYear IS NULL OR h.holidayYear = :holidayYear) AND " +
-           "(:countryCode IS NULL OR h.countryCode = :countryCode) AND " +
-           "(:from IS NULL OR h.date >= :from) AND " +
-           "(:to IS NULL OR h.date <= :to) AND " +
-           "(:type IS NULL OR :type MEMBER OF h.types)")
-    Page<Holiday> searchHolidays(@Param("holidayYear") Integer holidayYear,
-                                @Param("countryCode") String countryCode,
-                                @Param("from") LocalDate from,
-                                @Param("to") LocalDate to,
-                                @Param("type") String type,
-                                Pageable pageable);
+public interface HolidayRepository extends JpaRepository<Holiday, Long>, HolidayCustomRepository {
     
     @Modifying
     @Query("DELETE FROM Holiday h WHERE h.countryCode = :countryCode AND h.holidayYear = :holidayYear")
