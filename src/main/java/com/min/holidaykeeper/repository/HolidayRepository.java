d99d1da9e3162c2ec06d223f76bcab9b8a8ba9b6
Gdm0714 <ehdals45454@naver.com>
2025-06-21 10:00:12 +0900

    refactor: 필요없는 import 제거

--- src/main/java/com/min/holidaykeeper/repository/HolidayRepository.java
+++ src/main/java/com/min/holidaykeeper/repository/HolidayRepository.java
@@ -9,11 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface HolidayRepository extends JpaRepository<Holiday, Long> {
-    
-
     @Query("SELECT h FROM Holiday h WHERE " +
            "(:holidayYear IS NULL OR h.holidayYear = :holidayYear) AND " +
            "(:countryCode IS NULL OR h.countryCode = :countryCode) AND " +
