8336a376c7bd8c3d90dd75e18398f4e6bc3e1aed
Gdm0714 <ehdals45454@naver.com>
2025-06-21 11:06:50 +0900

    fix: deprecated 메서드 수정

--- src/main/java/com/min/holidaykeeper/config/NagerRestTemplateConfig.java
+++ src/main/java/com/min/holidaykeeper/config/NagerRestTemplateConfig.java
@@ -3,19 +3,19 @@ package com.min.holidaykeeper.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 
 @Configuration
 public class NagerRestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
@@ -23,10 +23,9 @@ public class NagerRestTemplateConfig {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper);
         
-        return builder
-                .setConnectTimeout(Duration.ofSeconds(10))
-                .setReadTimeout(Duration.ofSeconds(30))
-                .messageConverters(converter)
-                .build();
+        restTemplate.getMessageConverters().removeIf(m -> m instanceof MappingJackson2HttpMessageConverter);
+        restTemplate.getMessageConverters().add(converter);
+        
+        return restTemplate;
     }
 }
