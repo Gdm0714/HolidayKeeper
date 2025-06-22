7a1fcd299aa2095d0d7b053a9cc272ba29d00389
Gdm0714 <ehdals45454@naver.com>
2025-06-22 18:05:23 +0900

    feat: text/json Content-Type 처리할 수 있게 추가

--- src/main/java/com/min/holidaykeeper/config/NagerRestTemplateConfig.java
+++ src/main/java/com/min/holidaykeeper/config/NagerRestTemplateConfig.java
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
+import java.util.Arrays;
 
 @Configuration
 public class NagerRestTemplateConfig {
@@ -22,6 +23,13 @@ public class NagerRestTemplateConfig {
         
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper);
+        
+        converter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.APPLICATION_JSON,
+                MediaType.APPLICATION_JSON_UTF8,
+                MediaType.valueOf("text/json"),
+                MediaType.valueOf("text/json;charset=utf-8")
+        ));
         
         restTemplate.getMessageConverters().removeIf(m -> m instanceof MappingJackson2HttpMessageConverter);
         restTemplate.getMessageConverters().add(converter);
